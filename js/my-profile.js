e3b9ad07e92de1bba7ca56f2e90e9c9fd562dcbf
kate0706 <68859986+kate0706@users.noreply.github.com>
2020-11-08 15:25:53 -0300

    Entrega 7

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,79 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+function savePerfil() {
 
-});
+    var nomb = document.getElementById("nombres").value;
+    var apell = document.getElementById("apellidos").value;
+    var age = document.getElementById("edad").value;
+    var tel = document.getElementById("telefono").value;
+    
+    sessionStorage.setItem("perfilUsuario",
+        JSON.stringify(
+        {
+        nombre: nomb,
+        apellido: apell,
+        edad: age,
+        usuario: localStorage.getItem("email"),
+        telefono: tel,
+        }
+        ));
+    
+    imprimirResultados();
+    }
+    
+    
+    document.addEventListener("DOMContentLoaded", function(e){
+        
+        var usuario = localStorage.getItem("email");
+        document.getElementById("usuario").value = usuario;
+    
+    
+    imprimirResultados();
+    
+    });
+    
+    function imprimirResultados() {
+    
+        let htmlContentToAppend = "";
+    
+        var data = JSON.parse(sessionStorage.getItem("perfilUsuario"));
+    
+            htmlContentToAppend += `
+
+                <div class="col">
+                <label for="nombres">
+                <input style="width: 400px" type="text" id="nombres" class="form-control" placeholder="Nombres" value="` + data.nombre + `" />
+                </label>
+              </div>
+                <div class="col">
+                <label for="apellidos">
+                <input style="width: 400px" type="text" id="apellidos" class="form-control" placeholder="Apellidos" value="` + data.apellido + `" />
+                </label>
+                </div>
+                <div class="col">
+                <label for="usuario">
+                <input style="width: 400px;" class="form-control" type="text" id="modEmail" value="` + localStorage.getItem("email") + `"/>
+                </label>
+            </div> 
+              <div class="col">
+                <label for="telefono">
+                <input style="width: 400px" type="number" id="telefono" class="form-control" placeholder="Teléfono" value="` + data.telefono + `" />
+                </label>
+              </div>
+              <div class="col">
+                <label for="edad">
+                <input style="width: 300px" type="number" id="edad" class="form-control" placeholder="Edad" value="` + data.edad + `" />
+                </label>
+                <br>
+                <div class="col">
+                <label for="guardarCambios" >
+                <input class="btn btn-primary btn-lg" type="submit" onclick="savePerfil()" id="updatePerfil" value="Guardar Cambios">
+                </label>
+                </div>
+        `
+    
+        document.getElementById("editarPerfil").innerHTML = htmlContentToAppend;
+    
+    }
+    
